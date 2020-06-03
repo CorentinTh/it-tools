4801e2f6c5ee0f5a779336b6032234d94c2c7b94
Corentin Thomasset <corentin.thomasset74@gmail.com>
2020-06-03 13:46:15 +0200

    feat: home page
    Signed-off-by: Corentin Thomasset <corentin.thomasset74@gmail.com>

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,16 +1,86 @@
 <template>
-  <v-card>
-    <v-card-text>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad at cumque dolore dolores, dolorum eius error est eum eveniet hic illum ipsum labore minus odio quibusdam repudiandae sed ullam veritatis!
-    </v-card-text>
-  </v-card>
+    <v-card class="single-card">
+        <v-card-text>
+            <div class="card-wrapper ">
+                <div v-for="item in items" :key="item.text" class="pa-2">
+                    <v-card class="card-element" color="primary" :to="item.path">
+                        <v-card-text>
+                            <v-icon>{{ item.icon }}</v-icon>
+                            <div class="item-title">{{item.text}}</div>
+                        </v-card-text>
+                    </v-card>
+                </div>
+            </div>
+        </v-card-text>
+    </v-card>
 </template>
 
 <script>
+    import {toolsComponentsFlat} from "../router";
 
-export default {
-  name: 'Home',
-  components: {
-  }
-}
+    export default {
+        name: 'Home',
+        data: () => ({
+            items: toolsComponentsFlat
+        })
+    }
 </script>
+
+<style lang="less" scoped>
+    .card-wrapper {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+
+        div {
+            flex: 1 1 33%;
+
+            @media only screen and (max-width: 620px) {
+                flex: 0 1 50%;
+            }
+            @media only screen and (max-width: 450px) {
+                flex: 0 1 220px;
+            }
+            @media only screen and (max-width: 300px) {
+                flex: 0 1 100%;
+            }
+
+            .card-element {
+                border-radius: 24px 4px !important;
+                color: #ffffff !important;
+                font-weight: bold !important;
+                text-align: center;
+                transition: 0.2s ease;
+                position: relative;
+                top: 0;
+
+                &:hover {
+                    position: relative;
+                    top: -5px;
+                    opacity: 0.9;
+                }
+
+                .v-icon {
+                    font-size: 50px;
+                    color: #ffffff;
+                    margin: 15px 0;
+                    transition: 0.3s cubic-bezier(0.25, 0.8, 0.5, 1);
+
+                }
+
+                .item-title {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    font-size: 18px;
+                    line-height: 22px;
+                    height: 44px;
+                    opacity: 1;
+                    color: #ffffff;
+                    transition: 0.3s cubic-bezier(0.25, 0.8, 0.5, 1);
+
+                }
+            }
+        }
+    }
+</style>
