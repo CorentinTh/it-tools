7b741783c3993e1515dd95bf8a6ccb4403a22b2b
Corentin Thomasset <corentin.thomasset74@gmail.com>
2021-05-21 23:54:07 +0200

    chore(ci): commitlint increase body length

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [2, 'always', Infinity]
+  }
 }
