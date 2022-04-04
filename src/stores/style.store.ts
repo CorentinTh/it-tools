40e9af06cf28b7348152f8ec3898fa2b27ec0b21
Corentin Thomasset <corentin.thomasset74@gmail.com>
2022-04-04 17:27:34 +0200

    feat: persistent theme selection fallback to prefered theme

--- src/stores/style.store.ts
+++ src/stores/style.store.ts
@@ -1,5 +1,13 @@
+import { useStorage, usePreferredDark } from '@vueuse/core';
 import { defineStore } from 'pinia';
+import type { Ref } from 'vue';
 
-export const useStyleStore = defineStore('style', () => ({
-  isDarkTheme: true,
-}));
+export const useStyleStore = defineStore('style', {
+  state: () => {
+    const isDark = usePreferredDark();
+
+    return {
+      isDarkTheme: useStorage('useDarkTheme', isDark) as Ref<boolean>,
+    };
+  },
+});
