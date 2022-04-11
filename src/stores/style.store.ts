3ae872847b00d65e4e2e629775d479a3333450f1
Corentin Thomasset <corentin.thomasset74@gmail.com>
2022-04-11 13:59:15 +0200

    fix(style): working dark mode persistence

--- src/stores/style.store.ts
+++ src/stores/style.store.ts
@@ -1,13 +1,9 @@
-import { useStorage, usePreferredDark } from '@vueuse/core';
+import { useStorage } from '@vueuse/core';
 import { defineStore } from 'pinia';
 import type { Ref } from 'vue';
 
 export const useStyleStore = defineStore('style', {
-  state: () => {
-    const isDark = usePreferredDark();
-
-    return {
-      isDarkTheme: useStorage('useDarkTheme', isDark) as Ref<boolean>,
-    };
-  },
+  state: () => ({
+    isDarkTheme: useStorage('useDarkTheme', false) as Ref<boolean>,
+  }),
 });
