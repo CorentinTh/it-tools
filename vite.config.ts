889d59499212a449ee460c68c480648e337a7ecb
Corentin Thomasset <corentin.thomasset74@gmail.com>
2022-04-12 12:51:21 +0200

    fix: removed global define

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
-  define: {
-    global: {},
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
