65a6896563d16f30420424e274bd306e3e9182c8
Corentin Thomasset <corentin.thomasset74@gmail.com>
2022-04-14 23:12:19 +0200

    refactor(style): improve style for tool-card

--- src/components/ToolCard.vue
+++ src/components/ToolCard.vue
@@ -1,10 +1,12 @@
 <template>
     <router-link :to="tool.path">
         <n-card class="tool-card">
-            <n-icon class="icon" size="35">
+            <n-icon class="icon" size="40">
                 <component :is="tool.icon" />
             </n-icon>
-            <n-h3 class="title">{{ tool.name }}</n-h3>
+            <n-h3 class="title">
+                <n-ellipsis>{{ tool.name }}</n-ellipsis>
+            </n-h3>
             <div class="description">
                 <n-ellipsis :line-clamp="2" :tooltip="false">{{ tool.description }}</n-ellipsis>
             </div>
@@ -30,12 +32,15 @@ a {
     &:hover {
         border-color: var(--n-color-target);
     }
+
     .icon {
         opacity: 0.7;
     }
+
     .title {
         margin: 5px 0;
     }
+
     .description {
         opacity: 0.7;
         margin: 5px 0;
