2df3f53b78bbe419763fd359788a4b0b5710e4b7
Corentin Thomasset <corentin.thomasset74@gmail.com>
2022-04-14 18:59:19 +0200

    refactor(style): responsive layout

--- src/pages/Home.page.vue
+++ src/pages/Home.page.vue
@@ -5,7 +5,7 @@ import ToolCard from '../components/ToolCard.vue';
 
 <template>
     <div class="home-page">
-        <n-grid x-gap="12" y-gap="12" :cols="3">
+        <n-grid x-gap="12" y-gap="12" cols="1 400:2 800:3 1200:4 2000:8">
             <n-gi v-for="tool in toolsWithCategory" :key="tool.name">
                 <tool-card :tool="tool" />
             </n-gi>
