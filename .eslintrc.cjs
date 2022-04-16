f54492840220182b7e20f9ca4a38a035dea6ca7f
Corentin Thomasset <corentin.thomasset74@gmail.com>
2022-04-16 13:43:35 +0200

    chore: removed cypress integrations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,12 +7,6 @@ module.exports = {
   env: {
     'vue/setup-compiler-macros': true,
   },
-  overrides: [
-    {
-      files: ['cypress/integration/**.spec.{js,ts,jsx,tsx}'],
-      extends: ['plugin:cypress/recommended'],
-    },
-  ],
   rules: {
     'vue/multi-word-component-names': ['off'],
   },
