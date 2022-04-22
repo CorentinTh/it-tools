b22aa941f52009118d4d3cc98277cc4c402a4c77
Corentin Thomasset <corentin.thomasset74@gmail.com>
2022-04-23 00:43:42 +0200

    fix(sider): default collapsed value

--- src/stores/style.store.ts
+++ src/stores/style.store.ts
@@ -1,22 +1,14 @@
-import { useMediaQuery, useStorage, whenever } from '@vueuse/core';
+import { useMediaQuery, useStorage } from '@vueuse/core';
 import { defineStore } from 'pinia';
-import type { Ref } from 'vue';
+import { watch, type Ref } from 'vue';
 
 export const useStyleStore = defineStore('style', {
   state: () => {
     const isDarkTheme = useStorage('isDarkTheme', true) as Ref<boolean>;
     const isSmallScreen = useMediaQuery('(max-width: 700px)');
-    const isMenuCollapsed = useStorage('isMenuCollapsed', !isSmallScreen.value) as Ref<boolean>;
+    const isMenuCollapsed = useStorage('isMenuCollapsed', isSmallScreen.value) as Ref<boolean>;
 
-    whenever(
-      () => !isSmallScreen.value,
-      () => (isMenuCollapsed.value = false),
-    );
-
-    whenever(
-      () => isSmallScreen.value,
-      () => (isMenuCollapsed.value = true),
-    );
+    watch(isSmallScreen, (v) => (isMenuCollapsed.value = v));
 
     return {
       isDarkTheme,
