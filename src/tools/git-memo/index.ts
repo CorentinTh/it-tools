3cfc5f8bc27b66e6fbb6054f3c909818083ebc37
Corentin Thomasset <corentin.thomasset74@gmail.com>
2022-05-09 15:25:48 +0200

    fix(lint): missing new lines

--- src/tools/git-memo/index.ts
+++ src/tools/git-memo/index.ts
@@ -4,7 +4,8 @@ import type { ITool } from '../Tool';
 export const tool: ITool = {
   name: 'Git cheatsheet',
   path: '/git-memo',
-  description: 'Git is a decentralized version management software. With this cheatsheet you will have a quick access to the most common git commands.',
+  description:
+    'Git is a decentralized version management software. With this cheatsheet you will have a quick access to the most common git commands.',
   keywords: ['git', 'push', 'force', 'pull', 'commit', 'amend', 'rebase', 'merge', 'reset', 'soft', 'hard', 'lease'],
   component: () => import('./git-memo.vue'),
   icon: BrandGit,
--- src/tools/url-parser/index.ts
+++ src/tools/url-parser/index.ts
@@ -4,7 +4,8 @@ import type { ITool } from './../Tool';
 export const tool: ITool = {
   name: 'Url parser',
   path: '/url-parser',
-  description: 'Parse an url string to get all the different parts (protocol, origin, params, port, username-password, ...)',
+  description:
+    'Parse an url string to get all the different parts (protocol, origin, params, port, username-password, ...)',
   keywords: ['url', 'parser', 'protocol', 'origin', 'params', 'port', 'username', 'password', 'href'],
   component: () => import('./url-parser.vue'),
   icon: Unlink,
