92ce419f45e110509ab202485a36bf175ce345da
Corentin Thomasset <corentin.thomasset74@gmail.com>
2022-07-23 19:09:22 +0200

    fix(text-statistics): empty text mean 0 words and 0 lines

--- src/tools/text-statistics/text-statistics.vue
+++ src/tools/text-statistics/text-statistics.vue
@@ -5,19 +5,17 @@
     <br />
     <n-space justify="space-around">
       <n-statistic label="Character count" :value="text.length" />
-      <n-statistic label="Word count" :value="text.split(/\s+/).length" />
-      <n-statistic label="Line count" :value="text.split(/\r\n|\r|\n/).length" />
+      <n-statistic label="Word count" :value="text === '' ? 0 : text.split(/\s+/).length" />
+      <n-statistic label="Line count" :value="text === '' ? 0 : text.split(/\r\n|\r|\n/).length" />
       <n-statistic label="Byte size" :value="formatBytes(getStringSizeInBytes(text))" />
     </n-space>
   </n-card>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
 import { formatBytes } from '@/utils/convert';
+import { ref } from 'vue';
 import { getStringSizeInBytes } from './text-statistics.service';
 
-const text = ref(
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Commodo risus faucibus varius volutpat habitasse suspendisse justo inceptos primis mi. Fusce molestie lorem bibendum habitasse litora adipiscing turpis egestas quis nec. Non id conubia vulputate etiam iaculis vitae venenatis hac fusce condimentum. Adipiscing pellentesque venenatis ornare pulvinar tempus hac montes velit erat convallis.',
-);
+const text = ref('');
 </script>
