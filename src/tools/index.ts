214084262cec7fb881fd397626356b080ea1a5cc
Corentin Thomasset <corentin.thomasset74@gmail.com>
2022-07-23 23:04:57 +0200

    refactor(import): auto reordered imports

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,30 +1,30 @@
 import { LockOpen } from '@vicons/tabler';
 import type { ToolCategory } from './tool';
 
-import { tool as mathEvaluator } from './math-evaluator';
-import { tool as jsonViewer } from './json-viewer';
-import { tool as htmlEntities } from './html-entities';
-import { tool as urlParser } from './url-parser';
-import { tool as deviceInformation } from './device-information';
+import { tool as base64Converter } from './base64-converter';
 import { tool as bcrypt } from './bcrypt';
+import { tool as bip39 } from './bip39-generator';
 import { tool as caseConverter } from './case-converter';
 import { tool as colorConverter } from './color-converter';
-import { tool as qrCodeGenerator } from './qr-code-generator';
-import { tool as base64Converter } from './base64-converter';
 import { tool as crontabGenerator } from './crontab-generator';
+import { tool as dateTimeConverter } from './date-time-converter';
+import { tool as deviceInformation } from './device-information';
+import { tool as cypher } from './encryption';
+import { tool as gitMemo } from './git-memo';
+import { tool as hashText } from './hash-text';
+import { tool as htmlEntities } from './html-entities';
+import { tool as baseConverter } from './integer-base-converter';
+import { tool as jsonViewer } from './json-viewer';
+import { tool as loremIpsumGenerator } from './lorem-ipsum-generator';
+import { tool as mathEvaluator } from './math-evaluator';
+import { tool as qrCodeGenerator } from './qr-code-generator';
+import { tool as randomPortGenerator } from './random-port-generator';
+import { tool as romanNumeralConverter } from './roman-numeral-converter';
 import { tool as textStatistics } from './text-statistics';
 import { tool as tokenGenerator } from './token-generator';
-import { tool as hashText } from './hash-text';
-import { tool as uuidGenerator } from './uuid-generator';
-import { tool as romanNumeralConverter } from './roman-numeral-converter';
-import { tool as cypher } from './encryption';
-import { tool as bip39 } from './bip39-generator';
-import { tool as dateTimeConverter } from './date-time-converter';
-import { tool as gitMemo } from './git-memo';
-import { tool as baseConverter } from './integer-base-converter';
 import { tool as urlEncoder } from './url-encoder';
-import { tool as randomPortGenerator } from './random-port-generator';
-import { tool as loremIpsumGenerator } from './lorem-ipsum-generator';
+import { tool as urlParser } from './url-parser';
+import { tool as uuidGenerator } from './uuid-generator';
 
 export const toolsByCategory: ToolCategory[] = [
   {
