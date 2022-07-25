412de23796babbc080b0768a75029ff2ddf2acfc
Corentin Thomasset <corentin.thomasset74@gmail.com>
2022-07-25 23:31:06 +0200

    fix(base64-file): fixed url slug

--- src/tools/base64-file-converter/index.ts
+++ src/tools/base64-file-converter/index.ts
@@ -3,10 +3,9 @@ import { defineTool } from '../tool';
 
 export const tool = defineTool({
   name: 'Base64 file converter',
-  path: '/base64-converter',
+  path: '/base64-file-converter',
   description: "Convert string, files or images into a it's base64 representation.",
   keywords: ['base64', 'converter', 'upload', 'image', 'file', 'conversion', 'web', 'data', 'format'],
   component: () => import('./base64-file-converter.vue'),
   icon: FileDigit,
-  redirectFrom: ['/file-to-base64', '/base64-string-converter'],
 });
