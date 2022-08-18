49755909bdaea9399e51b67fbd1a6d071acd3182
Corentin Thomasset <corentin.thomasset74@gmail.com>
2022-08-18 10:53:23 +0200

    fix(deps): added missing optional deps

--- src/utils/boolean.test.ts
+++ src/utils/boolean.test.ts
@@ -1,10 +1,11 @@
+import _ from 'lodash';
 import { describe, expect, it } from 'vitest';
 import { isNotThrowing } from './boolean';
 
 describe('boolean utils', () => {
   describe('isNotThrowing', () => {
     it('should return if the call throws or false otherwise', () => {
-      expect(isNotThrowing(() => {})).to.eql(true);
+      expect(isNotThrowing(_.noop)).to.eql(true);
       expect(
         isNotThrowing(() => {
           throw new Error();
