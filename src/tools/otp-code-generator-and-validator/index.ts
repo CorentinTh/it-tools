7f229959d64b7a932f32753e3838d87a819a9192
Corentin Thomasset <corentin.thomasset74@gmail.com>
2022-08-24 00:22:54 +0200

    refactor(otp-generator): changed url

--- src/tools/otp-code-generator-and-validator/index.ts
+++ src/tools/otp-code-generator-and-validator/index.ts
@@ -3,7 +3,7 @@ import { defineTool } from '../tool';
 
 export const tool = defineTool({
   name: 'OTP code generator',
-  path: '/otp-code-generator-and-validator',
+  path: '/otp-generator',
   description: 'Generate and validate time-based OTP (one time password) for multi-factor authentication.',
   keywords: [
     'otp',
