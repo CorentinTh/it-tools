6e84ea40616ab95d90f7ff6f8ab1f7723dea7112
Corentin Thomasset <corentin.thomasset74@gmail.com>
2023-04-05 22:55:40 +0200

    feat(new-tool): simple benchmark calculator

---
+++ src/tools/benchmark-builder/index.ts
@@ -0,0 +1,11 @@
+import { SpeedFilled } from '@vicons/material';
+import { defineTool } from '../tool';
+
+export const tool = defineTool({
+  name: 'Benchmark builder',
+  path: '/benchmark-builder',
+  description: 'Easily compare execution time of tasks with this very simple online benchmark builder.',
+  keywords: ['benchmark', 'builder', 'execution', 'duration', 'mean', 'variance'],
+  component: () => import('./benchmark-builder.vue'),
+  icon: SpeedFilled,
+});
