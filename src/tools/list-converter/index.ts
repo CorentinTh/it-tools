06b4aef422577c0c4c4cd6121e5619294dec4f65
Corentin THOMASSET <corentin.thomasset74@gmail.com>
2023-05-07 11:24:27 +0200

    Update src/tools/list-converter/index.ts

--- src/tools/list-converter/index.ts
+++ src/tools/list-converter/index.ts
@@ -8,6 +8,6 @@ export const tool = defineTool({
     'This tool can process column-based data and apply various changes (transpose, add prefix and suffix, reverse list, sort list, lowercase values, truncate values) to each row.',
   keywords: ['list', 'converter', 'sort', 'reverse', 'prefix', 'suffix', 'lowercase', 'truncate'],
   component: () => import('./list-converter.vue'),
-  icon: ArrowsShuffle,
+  icon: List,
   createdAt: new Date('2023-05-07'),
 });
