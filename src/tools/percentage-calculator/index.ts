265860e993d5dd2eceda18680afc5acc12f36fbe
Seb <144435+rmtsrc@users.noreply.github.com>
2023-06-18 16:05:40 +0100

    Apply suggestions from code review
    Co-authored-by: Corentin THOMASSET <corentin.thomasset74@gmail.com>

--- src/tools/percentage-calculator/index.ts
+++ src/tools/percentage-calculator/index.ts
@@ -4,8 +4,9 @@ import { defineTool } from '../tool';
 export const tool = defineTool({
   name: 'Percentage calculator',
   path: '/percentage-calculator',
-  description: 'Calculate percentages',
-  keywords: ['percentage', 'calculator'],
+  description: 'Easily calculate percentages from a value to another value, or from a percentage to a value.',
+  keywords: ['percentage', 'calculator', 'calculate', 'value', 'number', '%'],
   component: () => import('./percentage-calculator.vue'),
   icon: Percentage,
+  createdAt: new Date('2023-06-18'),
 });
