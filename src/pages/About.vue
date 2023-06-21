7068610438a8e0caed8e85c855f5db04dea5effd
Marvin <33938500+marvin-j97@users.noreply.github.com>
2023-06-21 21:45:48 +0200

    fix(about): typos and wording (#474)
    * fix(About): typos and wording
    
    * Update src/pages/About.vue
    
    ---------
    
    Co-authored-by: Corentin THOMASSET <corentin.thomasset74@gmail.com>

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -14,12 +14,12 @@ const { tracker } = useTracker();
       <c-link href="https://github.com/CorentinTh" target="_blank" rel="noopener">
         Corentin Thomasset
       </c-link>,
-      aggregates useful tools for developer and people working in IT. If you find it useful, please fell free to share
-      it to people you think may find it useful too and don't forget to pin it in your shortcut bar !
+      aggregates useful tools for developer and people working in IT. If you find it useful, please feel free to share
+      it to people you think may find it useful too and don't forget to bookmark it in your shortcut bar!
     </n-p>
     <n-p>
-      IT Tools is open-source (under the MIT license) and free, and will always be, but it cost me money to host and
-      renew the domain name, if you want to support my work, and encourage me to add more tools, please consider
+      IT Tools is open-source (under the MIT license) and free, and will always be, but it costs me money to host and
+      renew the domain name. If you want to support my work, and encourage me to add more tools, please consider
       supporting by
       <c-link
         href="https://www.buymeacoffee.com/cthmsst"
@@ -33,8 +33,8 @@ const { tracker } = useTracker();
 
     <n-h2>Technologies</n-h2>
     <n-p>
-      IT Tools is made in Vue JS (vue 3) with the the naive-ui component library and is hosted and continuously deployed
-      by Vercel. Third party open-source libraries are used in some tools, you may find the complete list in the
+      IT Tools is made in Vue.js (Vue 3) with the the Naive UI component library and is hosted and continuously deployed
+      by Vercel. Third-party open-source libraries are used in some tools, you may find the complete list in the
       <c-link href="https://github.com/CorentinTh/it-tools/blob/main/package.json" rel="noopener" target="_blank">
         package.json
       </c-link>
@@ -43,7 +43,7 @@ const { tracker } = useTracker();
 
     <n-h2>Found a bug? A tool is missing?</n-h2>
     <n-p>
-      If you need a tool that is currently not present here, and you think can be relevant, you are welcome to submit a
+      If you need a tool that is currently not present here, and you think can be useful, you are welcome to submit a
       feature request in the
       <c-link
         href="https://github.com/CorentinTh/it-tools/issues/new/choose"
@@ -55,7 +55,7 @@ const { tracker } = useTracker();
       in the GitHub repository.
     </n-p>
     <n-p>
-      And if you found a bug, or something broken that doesn't work as expected, please fill a bug report in the
+      And if you found a bug, or something doesn't work as expected, please fill a bug report in the
       <c-link
         href="https://github.com/CorentinTh/it-tools/issues/new/choose"
         rel="noopener"
