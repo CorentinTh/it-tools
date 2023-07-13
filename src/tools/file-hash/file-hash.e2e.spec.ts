b1341852e5526d32d083dcb7571739c168d54b0c
marvin-j97 <33938500+marvin-j97@users.noreply.github.com>
2023-07-14 01:56:13 +0200

    style: lint fix

--- src/tools/file-hash/file-hash.e2e.spec.ts
+++ src/tools/file-hash/file-hash.e2e.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test.describe('Tool - File hash', () => {
   test.beforeEach(async ({ page }) => {
--- src/tools/file-hash/file-hash.service.test.ts
+++ src/tools/file-hash/file-hash.service.test.ts
@@ -1,4 +1,3 @@
-import { expect, describe, it } from 'vitest';
 // import { } from './file-hash.service';
 //
 // describe('file-hash', () => {
