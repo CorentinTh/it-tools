35563b8457e06028746a09881164e0504838f59a
莫颓 <it_motui@163.com>
2023-10-22 16:07:43 +0800

    feat(i18n): 404 page (#679)

--- src/pages/404.page.vue
+++ src/pages/404.page.vue
@@ -11,17 +11,17 @@ useHead({ title: 'Page not found - IT Tools' });
     </span>
 
     <h1 m-0 mt-3>
-      404 Not Found
+      {{ $t('404.notFound') }}
     </h1>
     <div mt-4 op-60>
-      Sorry, this page does not seem to exist
+      {{ $t('404.sorry') }}
     </div>
     <div mb-8 op-60>
-      Maybe the cache is doing tricky things, try force-refreshing?
+      {{ $t('404.maybe') }}
     </div>
 
     <c-button to="/">
-      Back home
+      {{ $t('404.backHome') }}
     </c-button>
   </div>
 </template>
