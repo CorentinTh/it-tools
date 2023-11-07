e8bdf952977d9f2178242f783872f8145307aef1
elibrody2 <85048617+elibrody2@users.noreply.github.com>
2023-11-06 19:31:09 -0500

    Update Home.page.vue

--- src/pages/Home.page.vue
+++ src/pages/Home.page.vue
@@ -8,7 +8,7 @@ import { config } from '@/config';
 
 const toolStore = useToolStore();
 
-useHead({ title: 'Dev - IT Tools - Handy online tools for developers' });
+useHead({ title: 'IT Tools - Handy online tools for developers' });
 const { t } = useI18n();
 </script>
 
