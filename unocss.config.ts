478192065e6a251579a08089235ad1f26e0cc9c9
Corentin THOMASSET <corentin.thomasset74@gmail.com>
2023-11-12 23:22:41 +0100

    feat(new tool): pdf signature checker (#745)

--- unocss.config.ts
+++ unocss.config.ts
@@ -15,11 +15,13 @@ export default defineConfig({
   theme: {
     colors: {
       primary: '#1ea54c',
+
     },
   },
   shortcuts: {
     'pretty-scrollbar': 'scrollbar scrollbar-rounded scrollbar-thumb-color-gray-300 scrollbar-track-color-gray-100 dark:scrollbar-thumb-color-#424242 dark:scrollbar-track-color-#686868',
     'divider': 'h-1px bg-current op-10',
     'bg-surface': 'bg-#ffffff dark:bg-#232323',
+    'bg-background': 'bg-#f1f5f9 dark:bg-#1c1c1c',
   },
 });
