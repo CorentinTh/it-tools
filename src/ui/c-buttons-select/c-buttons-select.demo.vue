06dabe71fef1af7b383253001b97776406e9bc2f
Corentin Thomasset <corentin.thomasset74@gmail.com>
2023-11-13 22:30:16 +0100

    refactor(uuid generator): uuid version picker

--- src/ui/c-buttons-select/c-buttons-select.demo.vue
+++ src/ui/c-buttons-select/c-buttons-select.demo.vue
@@ -4,11 +4,18 @@ const optionsA = [
   { label: 'Option B', value: 'b', tooltip: 'This is a tooltip' },
   { label: 'Option C', value: 'c' },
 ];
+
+const optionB = {
+  'Option A': 'a',
+  'Option B': 'b',
+  'Option C': 'c',
+};
+
 const valueA = ref('a');
 </script>
 
 <template>
   <c-buttons-select v-model:value="valueA" :options="optionsA" label="Label: " />
   <c-buttons-select v-model:value="valueA" :options="optionsA" label="Label: " label-position="left" mt-2 />
-  <c-buttons-select v-model:value="valueA" :options="optionsA" label="Label: " label-position="left" mt-2 />
+  <c-buttons-select v-model:value="valueA" :options="optionB" label="Options object: " />
 </template>
