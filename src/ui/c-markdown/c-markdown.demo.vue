bd3edcb5281067051bbfaaff3e4a9177334707ea
Corentin THOMASSET <corentin.thomasset74@gmail.com>
2023-11-15 00:15:41 +0100

    refactor(about, i18n): improved i18n dx with markdown (#753)

---
+++ src/ui/c-markdown/c-markdown.demo.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const md = `
+# IT Tools
+
+## About
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis
+mollis blandit, nunc nisl aliquam nunc, vitae aliquam nisl nunc vitae nisl.
+
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+- Sed euismod, nisl quis mollis blandit, nunc nisl aliquam nunc, vitae aliquam nisl nunc vitae nisl.
+
+[it-tools](https://it-tools.tech)
+`;
+</script>
+
+<template>
+  <c-markdown :markdown="md" />
+</template>
