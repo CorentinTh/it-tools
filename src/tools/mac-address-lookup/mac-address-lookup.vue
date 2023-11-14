4b13351957cac126620462ab946e7778d525ba93
Corentin Thomasset <corentin.thomasset74@gmail.com>
2023-11-15 00:39:40 +0100

    chore(deps): switched from oui to oui-data for mac address lookup

--- src/tools/mac-address-lookup/mac-address-lookup.vue
+++ src/tools/mac-address-lookup/mac-address-lookup.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import db from 'oui/oui.json';
+import db from 'oui-data';
 import { macAddressValidationRules } from '@/utils/macAddress';
 import { useCopy } from '@/composable/copy';
 
 const getVendorValue = (address: string) => address.trim().replace(/[.:-]/g, '').toUpperCase().substring(0, 6);
 
 const macAddress = ref('20:37:06:12:34:56');
-const details = computed<string | undefined>(() => db[getVendorValue(macAddress.value)]);
+const details = computed<string | undefined>(() => (db as Record<string, string>)[getVendorValue(macAddress.value)]);
 
 const { copy } = useCopy({ source: () => details.value ?? '', text: 'Vendor info copied to the clipboard' });
 </script>
