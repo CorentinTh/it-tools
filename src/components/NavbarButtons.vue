7c4687f127f51418b47303d05091e44c828db292
elibrody <85048617+elibrody@users.noreply.github.com>
2024-01-10 18:41:24 -0500

    Update NavbarButtons.vue

--- src/components/NavbarButtons.vue
+++ src/components/NavbarButtons.vue
@@ -24,7 +24,7 @@ const { isDarkTheme } = toRefs(styleStore);
     <c-button
       circle
       variant="text"
-      href="https://twitter.com/elibrody"
+      href="https://twitter.com/is_that_merican"
       rel="noopener"
       target="_blank"
       :aria-label="$t('home.nav.twitterAccount')"
