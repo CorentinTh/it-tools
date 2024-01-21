39bc40172fa25d5e05bc3f82289578407de767e9
sharevb <sharevb@gmail.com>
2024-01-21 23:11:30 +0100

    feat(tool enhancement): Docker Run to Docker Compose
    - Ability to paste more than one docker run command
    - Ability to choose Docker Compose version
    - Ability to merge an existing Docker Compose
    - Ability to set indent size #775
    - Fix #814, #838

--- src/tools/docker-run-to-docker-compose-converter/index.ts
+++ src/tools/docker-run-to-docker-compose-converter/index.ts
@@ -4,7 +4,7 @@ import { defineTool } from '../tool';
 export const tool = defineTool({
   name: 'Docker run to Docker compose converter',
   path: '/docker-run-to-docker-compose-converter',
-  description: 'Turns docker run commands into docker-compose files!',
+  description: 'Turns docker run command(s) into docker-compose files!',
   keywords: ['docker', 'run', 'compose', 'yaml', 'yml', 'convert', 'deamon'],
   component: () => import('./docker-run-to-docker-compose-converter.vue'),
   icon: BrandDocker,
