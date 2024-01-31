c46207f1bb9fcd0691898be63bd20dfb5164de4b
hieudt-2054 <55786352+hieudt-2054@users.noreply.github.com>
2024-01-31 16:55:18 +0700

    feat(new-tool): added unicode conversion utilities (#858)
    * feat: add Text to Unicode tool
    
    * Update src/tools/text-to-unicode/index.ts
    
    ---------
    
    Co-authored-by: Corentin THOMASSET <corentin.thomasset74@gmail.com>

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,6 +1,7 @@
 import { tool as base64FileConverter } from './base64-file-converter';
 import { tool as base64StringConverter } from './base64-string-converter';
 import { tool as basicAuthGenerator } from './basic-auth-generator';
+import { tool as textToUnicode } from './text-to-unicode';
 import { tool as pdfSignatureChecker } from './pdf-signature-checker';
 import { tool as numeronymGenerator } from './numeronym-generator';
 import { tool as macAddressGenerator } from './mac-address-generator';
@@ -93,6 +94,7 @@ export const toolsByCategory: ToolCategory[] = [
       caseConverter,
       textToNatoAlphabet,
       textToBinary,
+      textToUnicode,
       yamlToJson,
       yamlToToml,
       jsonToYaml,
