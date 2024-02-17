c0f7f8fae9891dbeeb143157e8438719a7218c2b
sharevb <sharevb@gmail.com>
2024-02-17 17:32:52 +0100

    feat(new tool): ascii art generator
    Fix #829 ; Text ASCII Art Generator (using figlet)

---
+++ src/tools/ascii-text-drawer/index.ts
@@ -0,0 +1,12 @@
+import { Artboard } from '@vicons/tabler';
+import { defineTool } from '../tool';
+
+export const tool = defineTool({
+  name: 'ASCII Art Text Drawer',
+  path: '/ascii-text-drawer',
+  description: 'Draw a text using Ascii Art',
+  keywords: ['ascii', "asciiart", 'text', 'drawer'],
+  component: () => import('./ascii-text-drawer.vue'),
+  icon: Artboard,
+  createdAt: new Date('2024-02-17'),
+});
