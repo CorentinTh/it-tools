503cf6715c01992c5d7a0121908806d1437fecfe
sharevb <sharevb@gmail.com>
2024-02-17 17:28:58 +0100

    feat(rsa-key-generator): passphrase and formats
    Fix #281 (rsa with passphrase and comment)
    Supports many formats: PEM (with or without passphrase), PKCS#1, PKCS#8, OpenSSH Standard (with or without passphrase), OpenSSH (new format) and PuTTY

--- src/tools/rsa-key-pair-generator/index.ts
+++ src/tools/rsa-key-pair-generator/index.ts
@@ -5,8 +5,8 @@ import { translate } from '@/plugins/i18n.plugin';
 export const tool = defineTool({
   name: translate('tools.rsa-key-pair-generator.title'),
   path: '/rsa-key-pair-generator',
-  description: translate('tools.rsa-key-pair-generator.description'),
-  keywords: ['rsa', 'key', 'pair', 'generator', 'public', 'private', 'secret', 'ssh', 'pem'],
+  description: 'Generate new random RSA private and public keys (with or without passphrase).',
+  keywords: ['rsa', 'key', 'pair', 'generator', 'public', 'private', 'secret', 'ssh', 'pem', 'passphrase', 'password'],
   component: () => import('./rsa-key-pair-generator.vue'),
   icon: Certificate,
 });
--- src/tools/rsa-key-pair-generator/rsa-key-pair-generator.vue
+++ src/tools/rsa-key-pair-generator/rsa-key-pair-generator.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import type sshpk from 'sshpk';
 import { generateKeyPair } from './rsa-key-pair-generator.service';
 import TextareaCopyable from '@/components/TextareaCopyable.vue';
 import { withDefaultOnErrorAsync } from '@/utils/defaults';
@@ -6,7 +7,21 @@ import { useValidation } from '@/composable/validation';
 import { computedRefreshableAsync } from '@/composable/computedRefreshable';
 
 const bits = ref(2048);
-const emptyCerts = { publicKeyPem: '', privateKeyPem: '' };
+const comment = ref('');
+const password = ref('');
+const emptyCerts = { publicKey: '', privateKey: '' };
+
+const format = useStorage('rsa-key-pair-generator:format', 'ssh');
+const formatOptions = [
+  { value: 'pem', label: 'PEM' },
+  { value: 'pkcs1', label: 'PKCS#1' },
+  { value: 'pkcs8', label: 'PKCS#8' },
+  { value: 'ssh', label: 'OpenSSH Standard' },
+  { value: 'openssh', label: 'OpenSSH New' },
+  { value: 'putty', label: 'PuTTY' },
+];
+
+const supportsPassphrase = computed(() => format.value === 'pem' || format.value === 'ssh');
 
 const { attrs: bitsValidationAttrs } = useValidation({
   source: bits,
@@ -19,31 +34,67 @@ const { attrs: bitsValidationAttrs } = useValidation({
 });
 
 const [certs, refreshCerts] = computedRefreshableAsync(
-  () => withDefaultOnErrorAsync(() => generateKeyPair({ bits: bits.value }), emptyCerts),
+  () => withDefaultOnErrorAsync(() => generateKeyPair({
+    bits: bits.value,
+    password: password.value,
+    format: format.value as sshpk.PrivateKeyFormatType,
+    comment: comment.value,
+  }), emptyCerts),
   emptyCerts,
 );
 </script>
 
 <template>
-  <div style="flex: 0 0 100%">
-    <div item-style="flex: 1 1 0" style="max-width: 600px" mx-auto flex gap-3>
-      <n-form-item label="Bits :" v-bind="bitsValidationAttrs as any" label-placement="left" label-width="100">
+  <div>
+    <n-space justify="space-between" mb-1>
+      <c-select
+        v-model:value="format"
+        label-position="left"
+        label="Format:"
+        :options="formatOptions"
+        placeholder="Select a key format"
+      />
+
+      <n-form-item label="Bits :" v-bind="bitsValidationAttrs as any" label-placement="left">
         <n-input-number v-model:value="bits" min="256" max="16384" step="8" />
       </n-form-item>
+    </n-space>
 
+    <div v-if="supportsPassphrase" mb-1>
+      <n-form-item label="Passphrase :" label-placement="left">
+        <n-input
+          v-model:value="password"
+          type="password"
+          show-password-on="mousedown"
+          placeholder="Passphrase"
+        />
+      </n-form-item>
+    </div>
+
+    <div mb-1>
+      <n-form-item label="Comment :" label-placement="left">
+        <n-input
+          v-model:value="comment"
+          type="text"
+          placeholder="Comment"
+        />
+      </n-form-item>
+    </div>
+
+    <n-space justify="center" mb-1>
       <c-button @click="refreshCerts">
         Refresh key-pair
       </c-button>
+    </n-space>
+
+    <div>
+      <h3>Public key</h3>
+      <TextareaCopyable :value="certs.publicKey" :word-wrap="true" />
     </div>
-  </div>
 
-  <div>
-    <h3>Public key</h3>
-    <TextareaCopyable :value="certs.publicKeyPem" />
-  </div>
-
-  <div>
-    <h3>Private key</h3>
-    <TextareaCopyable :value="certs.privateKeyPem" />
+    <div>
+      <h3>Private key</h3>
+      <TextareaCopyable :value="certs.privateKey" />
+    </div>
   </div>
 </template>
