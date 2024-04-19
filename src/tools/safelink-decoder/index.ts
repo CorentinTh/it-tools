ce99ac4db1d301da5ed9b528b19fd21e1c75c2b1
zhu0823 <zhu0823@gmail.com>
2024-04-19 15:01:00 +0800

    fix(eslint): Replace double quotes with single quotes

--- src/tools/safelink-decoder/index.ts
+++ src/tools/safelink-decoder/index.ts
@@ -1,6 +1,6 @@
 import { Mailbox } from '@vicons/tabler';
 import { defineTool } from '../tool';
-import { translate } from "@/plugins/i18n.plugin";
+import { translate } from '@/plugins/i18n.plugin';
 
 export const tool = defineTool({
   name: translate('tools.outlook_safelink_decoder.title'),
