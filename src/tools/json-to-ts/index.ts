dd03874556d24a257d1e307c999728dbc67e7d15
ChenZhoYu <790348264@qq.com>
2024-04-25 15:56:48 +0800

    feat: json to typescript

---
+++ src/tools/json-to-ts/index.ts
@@ -0,0 +1,13 @@
+import { Braces } from '@vicons/tabler';
+import { defineTool } from '../tool';
+import { translate } from '@/plugins/i18n.plugin';
+
+export const tool = defineTool({
+  name: translate('tools.json-to-ts.title'),
+  path: '/json-to-ts',
+  description: translate('tools.json-to-ts.description'),
+  keywords: ['json', 'parse', 'typescript', 'convert', 'transform'],
+  component: () => import('./json-to-ts.vue'),
+  icon: Braces,
+  createdAt: new Date('2024-05-25'),
+});
