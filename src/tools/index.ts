e46a5ec4d378eac7903761993e89a83befb2d9ec
sharevb <sharevb@gmail.com>
2024-04-28 12:30:03 +0200

    feat(new tool): HDD Size Calculator
    Fix #974

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -6,6 +6,7 @@ import { tool as asciiTextDrawer } from './ascii-text-drawer';
 
 import { tool as textToUnicode } from './text-to-unicode';
 import { tool as safelinkDecoder } from './safelink-decoder';
+import { tool as hddCalculator } from './hdd-calculator';
 import { tool as pdfSignatureChecker } from './pdf-signature-checker';
 import { tool as numeronymGenerator } from './numeronym-generator';
 import { tool as macAddressGenerator } from './mac-address-generator';
@@ -107,6 +108,7 @@ export const toolsByCategory: ToolCategory[] = [
       listConverter,
       tomlToJson,
       tomlToYaml,
+      hddCalculator,
     ],
   },
   {
