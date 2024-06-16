11206a52a19004c6bd8493397fb24347fc2df4c1
ShareVB <sharevb@gmail.com>
2024-06-16 13:55:40 +0200

    fix(Mac Address Converter): update description

--- src/tools/mac-address-converter/index.ts
+++ src/tools/mac-address-converter/index.ts
@@ -2,14 +2,18 @@ import { Devices } from '@vicons/tabler';
 import { defineTool } from '../tool';
 
 export const tool = defineTool({
-  name: 'Mac address converter',
+  name: 'MAC Address Converter',
   path: '/mac-address-converter',
-  description: 'Change the format of a MAC address and chose between different formats',
+  description: 'Change the format of a MAC address and chose between different formats (EUI-48, EUI-64, IPv6)',
   keywords: [
     'converter',
     'mac',
     'address',
     'format',
+    'link-local',
+    'ipv6',
+    'eui-48',
+    'eui-64',
   ],
   component: () => import('./mac-address-converter.vue'),
   icon: Devices,
