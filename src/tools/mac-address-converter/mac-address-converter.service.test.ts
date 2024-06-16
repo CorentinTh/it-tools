c45a0556430df551351330b3622a4bbaaaedaa1e
ShareVB <sharevb@gmail.com>
2024-06-16 13:57:24 +0200

    fix: nosonar in unit tests

--- src/tools/mac-address-converter/mac-address-converter.service.test.ts
+++ src/tools/mac-address-converter/mac-address-converter.service.test.ts
@@ -18,8 +18,8 @@ describe('mac-address-converter', () => {
     expect(convertMacCanonicalIEEE(macValue)).to.equal('00-0A-95-9D-68-16');
     expect(convertMacCISCO(macValue)).to.equal('000a.959d.6816');
 
-    expect(convertMacToEUI64CanonicalIETF(macValue, true)).to.equal('02:0a:95:ff:fe:9d:68:16');
-    expect(convertMacToEUI64CanonicalIETF(macValue, false)).to.equal('00:0a:95:ff:fe:9d:68:16');
+    expect(convertMacToEUI64CanonicalIETF(macValue, true)).to.equal('02:0a:95:ff:fe:9d:68:16'); // NOSONAR
+    expect(convertMacToEUI64CanonicalIETF(macValue, false)).to.equal('00:0a:95:ff:fe:9d:68:16'); // NOSONAR
     expect(convertMacToEUI64CanonicalIEEE(macValue, true)).to.equal('02-0A-95-FF-FE-9D-68-16');
     expect(convertMacToEUI64CanonicalIEEE(macValue, false)).to.equal('00-0A-95-FF-FE-9D-68-16');
     expect(convertMacToEUI64CISCO(macValue, true)).to.equal('020a.95ff.fe9d.6816');
