318fb6efb99fb916aee0418b7767a05a382b6275
Corentin THOMASSET <corentin.thomasset74@gmail.com>
2024-08-15 15:29:58 +0200

    feat(new-tool): add email normalizer (#1243)

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,6 +1,7 @@
 import { tool as base64FileConverter } from './base64-file-converter';
 import { tool as base64StringConverter } from './base64-string-converter';
 import { tool as basicAuthGenerator } from './basic-auth-generator';
+import { tool as emailNormalizer } from './email-normalizer';
 
 import { tool as asciiTextDrawer } from './ascii-text-drawer';
 
@@ -152,6 +153,7 @@ export const toolsByCategory: ToolCategory[] = [
       dockerRunToDockerComposeConverter,
       xmlFormatter,
       yamlViewer,
+      emailNormalizer,
     ],
   },
   {
