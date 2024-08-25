8fab512a8f6fbf058dddf2022b788c16b281a937
Corentin THOMASSET <corentin.thomasset74@gmail.com>
2024-08-25 22:52:24 +0200

    Update src/tools/markdown-to-html/index.ts

--- src/tools/markdown-to-html/index.ts
+++ src/tools/markdown-to-html/index.ts
@@ -2,11 +2,11 @@ import { Markdown } from '@vicons/tabler';
 import { defineTool } from '../tool';
 
 export const tool = defineTool({
-  name: 'Markdown to Html',
+  name: 'Markdown to HTML',
   path: '/markdown-to-html',
   description: 'Convert Markdown to Html and allow to print (as PDF)',
   keywords: ['markdown', 'html', 'converter', 'pdf'],
   component: () => import('./markdown-to-html.vue'),
   icon: Markdown,
-  createdAt: new Date('2024-08-15'),
+  createdAt: new Date('2024-08-25'),
 });
