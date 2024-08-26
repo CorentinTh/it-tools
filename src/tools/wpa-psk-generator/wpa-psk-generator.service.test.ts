96f6262392f694ba3871ef1c0f2641352292a189
ShareVB <sharevb@gmail.com>
2024-08-26 17:49:40 +0200

    fix: wrong key length

--- src/tools/wpa-psk-generator/wpa-psk-generator.service.test.ts
+++ src/tools/wpa-psk-generator/wpa-psk-generator.service.test.ts
@@ -5,8 +5,9 @@ describe('wpa-psk-generator', () => {
   it('generateWpaPskRawKey should generate raw key', () => {
     expect(generateWpaPskRawKey('test', 'test')).to.deep.eq({
       passphrase: 'test',
-      psk: 'd630c5513becfd3952432bd7fcf098b7a40907f3214cf43551f1b8cfda873eccd55e2e0c6b8fed55feecdd7f21db4fb6b31c602fe3f5e58e7edd462b12e4acc4632aa41c4755646b8a52826cb76f3a984571c4cfc73a1a2684f55790fac9e1f6c6002faedcb6c2d47a3678139027b95641efbcecd934b712bf48db71a76d8915',
+      psk: 'd630c5513becfd3952432bd7fcf098b7a40907f3214cf43551f1b8cfda873ecc',
       ssid: 'test',
     });
+    expect(generateWpaPskRawKey('test', 'test')?.psk).toHaveLength(256 / 8 * 2);
   });
 });
--- src/tools/wpa-psk-generator/wpa-psk-generator.service.ts
+++ src/tools/wpa-psk-generator/wpa-psk-generator.service.ts
@@ -3,7 +3,7 @@ import pbkdf2 from 'crypto-js/pbkdf2';
 
 export function generateWpaPskRawKey(ssid: string, passphrase: string) {
   const psk = pbkdf2(passphrase, ssid, {
-    keySize: 32,
+    keySize: 256 / 32,
     iterations: 4096,
     hasher: CryptoJS.algo.SHA1,
   }).toString(CryptoJS.enc.Hex);
