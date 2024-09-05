ab7b3a6588f9d1f05db5286a722717b359b37177
steffenrapp <88974099+steffenrapp@users.noreply.github.com>
2024-09-05 17:21:43 +0000

    copy

--- src/tools/text-statistics/text-statistics.vue
+++ src/tools/text-statistics/text-statistics.vue
@@ -8,7 +8,7 @@ const { t } = useI18n();
 
 <template>
   <c-card>
-    <c-input-text v-model:value="text" multiline placeholder="Your text..." rows="5" />
+    <c-input-text v-model:value="text" multiline :placeholder="t('tools.text-statistics.placeholder')" rows="5" />
 
     <div mt-5 flex>
       <n-statistic :label="t('tools.text-statistics.characters')" :value="text.length" flex-1 />
