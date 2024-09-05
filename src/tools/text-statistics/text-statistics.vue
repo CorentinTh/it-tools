917ff521ea6073e5fbf4862e3af5f81fbc6747ef
steffenrapp <88974099+steffenrapp@users.noreply.github.com>
2024-09-05 17:01:34 +0000

    more tools

--- src/tools/text-statistics/text-statistics.vue
+++ src/tools/text-statistics/text-statistics.vue
@@ -3,6 +3,7 @@ import { getStringSizeInBytes } from './text-statistics.service';
 import { formatBytes } from '@/utils/convert';
 
 const text = ref('');
+const { t } = useI18n();
 </script>
 
 <template>
@@ -10,10 +11,10 @@ const text = ref('');
     <c-input-text v-model:value="text" multiline placeholder="Your text..." rows="5" />
 
     <div mt-5 flex>
-      <n-statistic label="Character count" :value="text.length" flex-1 />
-      <n-statistic label="Word count" :value="text === '' ? 0 : text.split(/\s+/).length" flex-1 />
-      <n-statistic label="Line count" :value="text === '' ? 0 : text.split(/\r\n|\r|\n/).length" flex-1 />
-      <n-statistic label="Byte size" :value="formatBytes(getStringSizeInBytes(text))" flex-1 />
+      <n-statistic :label="t('tools.text-statistics.characters')" :value="text.length" flex-1 />
+      <n-statistic :label="t('tools.text-statistics.words')" :value="text === '' ? 0 : text.split(/\s+/).length" flex-1 />
+      <n-statistic :label="t('tools.text-statistics.lines')" :value="text === '' ? 0 : text.split(/\r\n|\r|\n/).length" flex-1 />
+      <n-statistic :label="t('tools.text-statistics.bytes')" :value="formatBytes(getStringSizeInBytes(text))" flex-1 />
     </div>
   </c-card>
 </template>
