e3e8e9d4e4bbc7f1cb57064363eac0076a666f9a
sharevb <sharevb@gmail.com>
2024-09-28 11:22:56 +0200

    feat(Html WYSYWIG): add colors and tables
    Fix #796

--- src/tools/html-wysiwyg-editor/editor/menu-bar-item.vue
+++ src/tools/html-wysiwyg-editor/editor/menu-bar-item.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import type { Component } from 'vue';
 
-const props = defineProps<{ icon: Component; title: string; action: () => void; isActive?: () => boolean }>();
-const { icon, title, action, isActive } = toRefs(props);
+const props = defineProps<{ icon: Component; title: string; action: () => void; isActive?: () => boolean; enabled?: () => boolean }>();
+const { icon, title, action, isActive, enabled } = toRefs(props);
 </script>
 
 <template>
   <c-tooltip :tooltip="title">
-    <c-button circle variant="text" :type="isActive?.() ? 'primary' : 'default'" @click="action">
+    <c-button circle variant="text" :disabled="enabled && !enabled()" :type="isActive?.() ? 'primary' : 'default'" @click="action">
       <n-icon :component="icon" />
     </c-button>
   </c-tooltip>
