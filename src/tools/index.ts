2384d3ba4499544a10095055c8b3164b981f5da3
sharevb <sharevb@gmail.com>
2024-09-28 11:09:35 +0200

    feat(new tool): Duration Calculator
    Fix #1037 and #1161

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,6 +1,7 @@
 import { tool as base64FileConverter } from './base64-file-converter';
 import { tool as base64StringConverter } from './base64-string-converter';
 import { tool as basicAuthGenerator } from './basic-auth-generator';
+import { tool as durationCalculator } from './duration-calculator';
 import { tool as pdfSignatureChecker } from './pdf-signature-checker';
 import { tool as numeronymGenerator } from './numeronym-generator';
 import { tool as macAddressGenerator } from './mac-address-generator';
@@ -151,7 +152,12 @@ export const toolsByCategory: ToolCategory[] = [
   },
   {
     name: 'Measurement',
-    components: [chronometer, temperatureConverter, benchmarkBuilder],
+    components: [
+      chronometer,
+      temperatureConverter,
+	  durationCalculator,
+      benchmarkBuilder,
+    ],
   },
   {
     name: 'Text',
