7170450aba5ee75fe658d6cd22c7e5aa277901f9
ShareVB <sharevb@gmail.com>
2024-10-07 22:24:08 +0200

    fix: better UI

--- src/tools/bounce-parser/bounce-parser.vue
+++ src/tools/bounce-parser/bounce-parser.vue
@@ -26,27 +26,41 @@ const parsedBounce = computed(() => {
       />
     </c-card>
 
-    <c-card v-if="parsedBounce && emailContent" title="Output">
-      <n-p v-if="parsedBounce.email?.bounce" style="color: green">
+    <c-card v-if="parsedBounce && emailContent" title="Output" mb-2>
+      <n-p v-if="parsedBounce.email?.email?.error" style="color: green" mb-2>
         This mail is a bounce email
       </n-p>
-      <n-p v-if="!parsedBounce.email?.bounce" style="color: red">
+      <n-p v-if="!parsedBounce.email?.email?.error" style="color: red" mb-2>
         This mail is NOT a bounce email
       </n-p>
-      <c-alert v-if="parsedBounce.parsingError">
+      <c-alert v-if="parsedBounce.parsingError" mb-2>
         {{ parsedBounce.parsingError }}
       </c-alert>
-      <input-copyable v-if="parsedBounce.email?.recipient" label="Recipient" :value="parsedBounce.email?.recipient" />
-      <input-copyable v-if="parsedBounce.email?.server?.hostname" label="Server (Host)" :value="parsedBounce.email?.server?.hostname" />
-      <input-copyable v-if="parsedBounce.email?.server?.ip" label="Server (IP)" :value="parsedBounce.email?.server?.ip" />
-      <input-copyable v-if="parsedBounce.email?.server?.port" label="Server (Port)" :value="parsedBounce.email?.server?.port" />
-      <input-copyable v-if="parsedBounce.email?.command" label="Recipient" :value="parsedBounce.email?.command" />
-      <c-card v-if="parsedBounce.email?.data" title="Details" mb-2>
-        <textarea-copyable :value="JSON.stringify(parsedBounce.email?.data, null, 2)" />
-      </c-card>
-      <c-card v-if="parsedBounce.email?.email?.error" title="Raw Error" mb-2>
-        <textarea-copyable :value="parsedBounce.email?.email?.error" />
-      </c-card>
+      <input-copyable v-if="parsedBounce.email?.data?.recipient" label="Recipient" :value="parsedBounce.email?.data?.recipient" mb-2 />
+      <div v-if="parsedBounce.email?.email?.error">
+        <c-card title="Error" mb-2>
+          <input-copyable v-if="parsedBounce.email?.data?.command" label="Command" :value="parsedBounce.email?.data?.command" />
+          <input-copyable v-if="parsedBounce.email?.data?.error?.code?.basic || parsedBounce.email?.data?.error?.code?.enhanced" label="Code" :value="`${parsedBounce.email?.data?.error?.code?.basic}/${parsedBounce.email?.data?.error?.code?.enhanced}`" />
+          <input-copyable v-if="parsedBounce.email?.data?.error?.label" label="Label" :value="parsedBounce.email?.data?.error?.label" />
+          <input-copyable v-if="parsedBounce.email?.data?.error?.type" label="Type" :value="parsedBounce.email?.data?.error?.type" />
+          <input-copyable v-if="parsedBounce.email?.data?.error?.temporary" label="Temporary" :value="parsedBounce.email?.data?.error?.temporary" />
+          <input-copyable v-if="parsedBounce.email?.data?.error?.permanent" label="Permanent" :value="parsedBounce.email?.data?.error?.permanent" />
+          <input-copyable v-if="parsedBounce.email?.data?.error?.data?.type" label="Subtype" :value="parsedBounce.email?.data?.error?.data?.type" />
+          <input-copyable v-if="parsedBounce.email?.data?.error?.data?.blocked" label="Blocked" :value="parsedBounce.email?.data?.error?.data?.blocked" />
+          <input-copyable v-if="parsedBounce.email?.data?.error?.data?.spam" label="Spam" :value="parsedBounce.email?.data?.error?.data?.spam" />
+        </c-card>
+        <c-card v-if="parsedBounce.email?.data?.server?.hostname || parsedBounce.email?.data?.server?.ip" title="Server" mb-2>
+          <input-copyable v-if="parsedBounce.email?.data?.server?.hostname" label="Host" :value="parsedBounce.email?.data?.server?.hostname" />
+          <input-copyable v-if="parsedBounce.email?.data?.server?.ip" label="IP" :value="parsedBounce.email?.data?.server?.ip" />
+          <input-copyable v-if="parsedBounce.email?.data?.server?.port" label="Port" :value="parsedBounce.email?.data?.server?.port" />
+        </c-card>
+        <c-card title="Details" mb-2>
+          <textarea-copyable :value="JSON.stringify(parsedBounce.email?.data, null, 2)" />
+        </c-card>
+        <c-card title="Raw Error" mb-2>
+          <textarea-copyable :value="parsedBounce.email?.email?.error" word-wrap />
+        </c-card>
+      </div>
     </c-card>
   </div>
 </template>
--- src/tools/bounce-parser/email-bounce-parser-browser.d.ts
+++ src/tools/bounce-parser/email-bounce-parser-browser.d.ts
@@ -2,16 +2,34 @@ declare module "email-bounce-parser-browser" {
     export default class EmailBounceParse {
         read(emailContent: string): {
             bounce: boolean
-            recipient?: string
-            data: any
-            command: string
-            server?: {
-                hostname: string
-                ip: string
-                port: string
-            }
             email?: {
+                body?: string
+                intro?: string
                 error?: string
+            }
+            data?: {
+                error?: {
+                code?: {
+                    basic?: string
+                    enhanced?:string
+                }
+                label?: string
+                type?: string
+                temporary?: boolean
+                permanent?: boolean
+                data?: {
+                    type?: string
+                    blocked?: boolean
+                    spam?: boolean
+                }
+                }
+                recipient?:string
+                server?: {
+                hostname?: string
+                ip?: string
+                port?: string
+                }
+                command?: string
             }
         }
     } 
