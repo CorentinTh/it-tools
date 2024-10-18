87b3e568aebb98c0dc190cc6469bcfebb6290f24
chadmin <chrishancode@gmail.com>
2024-10-17 17:50:07 -0700

    feat(text-diff) fix module not loading

--- src/tools/text-diff/text-diff.vue
+++ src/tools/text-diff/text-diff.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+import CDiffEditor from '@/ui/c-diff-editor/c-diff-editor.vue';
+</script>
+
 <template>
-  <c-card w-full important:flex-1 important:pa-0>
-    <c-diff-editor />
+  <c-card w-full important:flex-1 important:pa-1>
+    <CDiffEditor />
   </c-card>
 </template>
