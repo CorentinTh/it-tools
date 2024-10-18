e2498d6f90ccd8f9fe90f141c72953a4b1e41915
chadmin <chrishancode@gmail.com>
2024-10-18 11:28:08 -0700

    feat(json-to-csv) add example default value

--- src/tools/json-to-csv/json-to-csv.vue
+++ src/tools/json-to-csv/json-to-csv.vue
@@ -4,6 +4,8 @@ import { convertArrayToCsv } from './json-to-csv.service';
 import type { UseValidationRule } from '@/composable/validation';
 import { withDefaultOnError } from '@/utils/defaults';
 
+const defaultValue = '[\n   {\n      "Age": 18,\n      "Country": "Germany",\n      "Gender": "Male",\n      "Purchased": "N",\n      "Salary": 20000\n   },\n   {\n      "Age": 19,\n      "Country": "France",\n      "Gender": "Female",\n      "Purchased": "N",\n      "Salary": 22000\n   },\n   {\n      "Age": 20,\n      "Country": "England",\n      "Gender": "Female",\n      "Purchased": "N",\n      "Salary": 24000\n   }\n]';
+
 function transformer(value: string) {
   return withDefaultOnError(() => {
     if (value === '') {
@@ -24,6 +26,7 @@ const rules: UseValidationRule<string>[] = [
 <template>
   <format-transformer
     input-label="Your raw JSON"
+    :input-default="defaultValue"
     input-placeholder="Paste your raw JSON here..."
     output-label="CSV version of your JSON"
     :input-validation-rules="rules"
